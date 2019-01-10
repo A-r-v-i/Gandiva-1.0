e4e91caf16464930f468e1da9357d5cc4b404def
Aravind Sankar <babuaravindsv@gmail.com>
2019-01-10 11:54:47 +0530

    added vision,mission

--- js/main.js
+++ js/main.js
@@ -377,7 +377,7 @@
 
 function makeTimer() {
 
-    var endTime = new Date("4 February 2019 9:00:00 GMT+01:00");			
+    var endTime = new Date("5 February 2019 8:30:00 GMT+5.5");			
     endTime = (Date.parse(endTime) / 1000);
 
     var now = new Date();
